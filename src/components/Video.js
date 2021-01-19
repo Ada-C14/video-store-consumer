ed4368f264cfa0527f51e489cb43d65ec9984e6c
Marj E <marj@marjorieetienne.com>
2021-01-19 13:32:21 -0800

    added basic code for video component

---
+++ src/components/Video.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Video.css';
+
+const Video = () => {
+  return (
+    <div>
+      Video Component
+    </div>
+  )
+}
+
+Video.propTypes = {
+
+};
+
+export default Video;
