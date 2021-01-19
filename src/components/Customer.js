2bd66f112cb99e6e74ac5f4dfcc076cd429c6522
Marj E <marj@marjorieetienne.com>
2021-01-19 13:36:06 -0800

    added basic code for customer component

---
+++ src/components/Customer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Customer.css';
+
+const Customer = () => {
+  return (
+    <div>
+      Customer Component
+    </div>
+  )
+}
+
+Customer.propTypes = {
+
+};
+
+export default Customer;
