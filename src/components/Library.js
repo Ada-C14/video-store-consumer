e8e0686e5ebb4a839214a3dbcf31d5d57149496b
Marj E <marj@marjorieetienne.com>
2021-01-19 13:34:46 -0800

    added basic code for library component

---
+++ src/components/Library.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Library.css';
+
+const Library = () => {
+  return (
+    <div>
+      Library Component
+    </div>
+  )
+}
+
+Library.propTypes = {
+
+};
+
+export default Library;
