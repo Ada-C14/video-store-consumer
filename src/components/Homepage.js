fa73671eca302b3619603796697217421e075e06
Marj E <marj@marjorieetienne.com>
2021-01-19 15:32:29 -0800

    added code to Homepage component and solved error

---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div>
+      Homepage Component
+    </div>
+  )
+}
+
+Homepage.propTypes = {
+
+};
+
+export default Homepage;
