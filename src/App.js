a925ca1f2165c301b3871d9737e1db070aad40a5
Sophie Messing <sophie.e.messing@gmail.com>
2021-01-19 15:33:49 -0800

    created routes with React Router, set up basic app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+            <li>
+              <Link to="/library">Library</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/customers">
+            <Customers />
+          </Route>
+          <Route path="/library">
+            <Library />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
+
+function Library() {
+  return <h2>Video Library</h2>;
+
+}
+
 
 export default App;
