1c8d744b76a9786b13379d1954c103467fb68e70
Jasmine <jasylop@gmail.com>
2021-01-20 12:03:10 -0800

    set up state for video and customer and pass down state variables and functions down to child components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import Checkout from './components/Checkout'
 import './App.css';
-import Navigation from './components/Navigation.js';
-import Main from './components/Main.js';
 
 const App = () => {
-    // video state
-    // customers state
+    const [video, setVideo] = useState(null);
+    const [customer, setCustomer] = useState(null);
+
     return (
       <div className="App">
         <Navigation />
-        {/* Checkout component exist when video and customer state have values */}
-        {/* <Checkout />  */}
-        <Main />
+        <Checkout video={video} customer={customer} />
+        <Main videoCallback={setVideo} customerCallback={setCustomer}/>
       </div>
     );
-}
+};
 
 export default App;
