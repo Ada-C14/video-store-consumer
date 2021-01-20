2ee7926807e74cac5f8175b1e3dac7f80f63530a
Ida <ida.goitom@yahoo.com>
2021-01-20 13:41:40 -0800

    created Search page component

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Search = (props) => {
+  return (
+    <div className="video">
+      <ul className="video-details"></ul>
+    </div>
+  );
+};
+export default Search;
