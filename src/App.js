6976cbdf194c9d85a350c54e898918a6d205896f
Jessica Chan <jwinchan@gmail.com>
2021-01-20 05:02:29 -0800

    added axios install

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 import AddMovieForm from './components/AddMovieForm';
+
+const API_URL_BASE = 'http://localhost:3001/videos';
 
 class App extends Component {
   render() {
@@ -13,7 +15,7 @@ class App extends Component {
         </header>
       <AddMovieForm />
       </div>
-    );
+    );  
   }
 }
 
