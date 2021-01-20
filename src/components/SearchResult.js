7f69ccb405e6fad48abaa719cc36a9d3f62327cd
Li Dai <li.lea.dai@gmail.com>
2021-01-20 14:25:04 -0800

    search result formatting

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './Video.css';
 
 const SearchResult = (props) => {
 
-    return (
-        <div>
-            {props.result.title}
-        </div>
-    )
+  const selectThisVideo = (event) => {
+    event.preventDefault();
+    // props.result.onSelectVideoCallback(props.result.id)
+  }
+
+  return (
+    <div 
+      className={`container ${props.result.id === props.result.selectedVideo ? 'selected' : ''}`} 
+      onClick={selectThisVideo}
+    >
+      <section className='video-image'>
+        <img src={props.result.image_url} alt={`${props.result.title} movie cover`}/>
+      </section>
+      <section className='video-description'>
+        <h2>{props.result.title}</h2>
+        <h3>{props.result.releaseDate}</h3>
+        <p>{props.result.overview}</p>
+      </section>
+    </div>
+  )
 }
 
 export default SearchResult;
