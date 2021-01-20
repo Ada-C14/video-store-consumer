19321f7e33605f88012cdbd5f5b735d04efdeea8
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-19 19:25:33 -0800

    add Navbar component and update component name to Videos and VideoList

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './Video.css';
+
+const Video = (props) => {
+
+  const onVideoClick = () => {
+    props.onClickCallback(props.video)
+  }
+
+  return (
+    <button className="video-button" onClick={ onVideoClick }>
+      { props.video.title }
+    </button>
+  );
+}
+
+export default Video;
---
+++ src/components/VideoList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import './VideoList.css';
+import PropTypes from 'prop-types'
+import Video from './Video';
+const BASE_URL = 'http://localhost:3000/videos';
+const axios = require('axios');
+
+const VideoList = (props) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios.get(BASE_URL)
+
+    .then((response) => {
+      setVideos(response.data);
+    });
+
+  }, []);
+
+  const generateVideo = videos.map( video => {
+    return <Video key={ video.id } video={ video } onClickCallBack= { props.setSelectedVideoCallBack }/>
+  })
+
+  return (
+  <div className="VideoList">
+    <h3>Video List:</h3>
+    { generateVideo }
+  </div>);
+}
+
+export default VideoList;
