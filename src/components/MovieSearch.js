2514fefe2c855d7ade3e9fe449ab8cff65172d7a
Anna Kim <anna.kim93@gmail.com>
2021-01-20 12:28:57 -0500

    moviesearch comp: submit event handler; set state for alerts to pass to search results page

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import './MovieSearch.css';
 
 const MovieSearch = props => {
@@ -8,20 +9,30 @@ const MovieSearch = props => {
     searchResults: []
   });
 
+  const [alert, setAlert] = useState(null);
+
   const onInputChange = event => {
-    const newFormFields = { ...formFields }
+    const newFormFields = { ...formFields };
     newFormFields[event.target.name] = event.target.value;
     setFormFields(newFormFields);
   };
-
+  
   const onFormSubmit = event => {
     event.preventDefault();
-    // props.addCardCallback(formFields, targetBoard);
-    // setFormFields({
-    //   text: '',
-    //   emoji: ''
-    // });
-    // setTargetBoard(props.currentBoard);
+
+    const url = props.url + '/videos?query=' + formFields.searchQuery;
+
+    axios.get(url)
+      .then((response) => {
+        const newFormFields = { ...formFields };
+        const newQueryResults = response.data;
+        newFormFields.searchResults = newQueryResults;
+        setFormFields(newFormFields);
+        setAlert(`Found ${formFields.searchResults.length} results for your search:`);
+      })
+      .catch((error) => {
+        setAlert(error.message);
+      })
   };
 
   return (
