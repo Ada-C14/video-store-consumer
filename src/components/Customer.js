a97b19f66c0a03ed661210d9390a48ab97738215
Ren Carothers <rencarothers@gmail.com>
2021-01-19 19:39:10 -0800

    fixes customers component to display a list of customer names

---
+++ src/components/Customer.js
@@ -0,0 +1,14 @@
+const Customer = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+Customer.propTypes = {
+  // addCardCallback: PropTypes.func.isRequired
+  };
+  
+
+export default Customer;    
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Customer from './Customer';
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -13,8 +14,9 @@ const Customers = () => {
     axios.get(CUSTOMER_URL)
       .then((response) => {
         // Get the list of students
-        console.log(response.data);
+        // console.log(response.data);
         const apiCustomerResponse = response.data;
+        console.log(apiCustomerResponse[0].name)
         // Set the state
         setCustomersList(apiCustomerResponse);
       })
@@ -24,27 +26,48 @@ const Customers = () => {
       });
   }, []);
 
-  const customer = customersList.map(({customer}) => {
-    return (
+  // const generateCustomers = customersList.map(({customer}) => {
+  //   return (
+  //     <Customer
+  //       key={customer.id}
+  //       id={customer.id}
+  //       name={customer.name}
+  //     />
+  //   );
+  // });
+
+  const generateCustomers = (customers) => {
+    let customerComponentArray = [];
+    // console.log(cards)
+
+  for (const customer of customers) 
+  // console.log(card[`card`][`id`])
+  {
+    customerComponentArray.push(
       <Customer
-        key={customer.id}
-        id={customer.id}
-        name={customer.name}
+          key={customer.id}
+          id={customer.id}
+          name={customer.name}
+          // emoji={card[`card`][`emoji`]}
+          // deleteCard={deleteCard}
       />
-    );
-  });
+    )
+  }
+
+    return customerComponentArray;
+  }; 
 
     return (
       <div>
         Customer List
-        {customersList}
+        {generateCustomers(customersList)}
       </div>
     );
   }
 
-CustomerList.propTypes = {
+Customers.propTypes = {
   // addCardCallback: PropTypes.func.isRequired
   };
   
 
-export default CustomerList;
+export default Customers;
