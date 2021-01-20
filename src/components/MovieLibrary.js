86f2eb863e257b30b79ec71a86deb3b535c63cb3
Anna Kim <anna.kim93@gmail.com>
2021-01-20 13:54:35 -0500

    add similar styling to movie library list

--- src/components/MovieLibrary.js
+++ src/components/MovieLibrary.js
@@ -1,40 +1,39 @@
 import { useEffect, useState } from 'react';
+import Movie from './Movie.js'
 import './MovieLibrary.css';
+import './MovieSearchResults.css';
 import propTypes from 'prop-types';
 import axios from 'axios';
 
 // From the video library page, I can see a list of all videos in the video library
 
 export default function MovieLibrary() {
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
 
-    const [movies, setMovies] = useState([])
-    const [errorMessage, setErrorMessage] = useState(null)
+  useEffect(() => {
+    axios.get('http://localhost:3000/videos')
+      .then((response) => {
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/videos')
-            .then((response) => {
-                console.log(response)
-                setMovies(
-                    response.data
-                );
-            })
-            .catch((error) => {
-                setErrorMessage(error.message);
-            });
-    }, []);
-
-    return (
-        <div className='list'>
-            <ul>
-                {movies.map((movie) => 
-                <li key={movie.id}>
-                    {movie.title}
-                </li>)}
-            </ul>
-            {errorMessage ? <div><h2 className="error-display">{errorMessage}</h2></div> : ''}
-        </div>
-
-    )
-
+  return (
+    <div className='search-results-container'>
+      {movies.map((movie) => 
+        <Movie 
+          key={movie.external_id}  
+          title={movie.title} 
+          overview={movie.overview}
+          releaseDate={movie.release_date}
+          imageURL={movie.image_url}
+        />
+      )}
+      {errorMessage ? <div><h2 className="error-display">{errorMessage}</h2></div> : ''}
+    </div>
+  )
 }
 
