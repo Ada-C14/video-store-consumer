32b6a8bad3e46d281039f3b2234ea45fef2cb078
roshni-patel <rpatel01@wesleyan.edu>
2021-01-20 17:36:01 -0600

    Set up react router

---
+++ src/components/Customer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Customer = () => {
+    return <h1>This is the customer page.</h1>
+}
+
+export default Customer; 
---
+++ src/components/Library.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState} from 'react'; 
+// import axios from 'axios';
+
+
+const Library = () => {
+//   const [videoList, setVideoList] = useState([]);
+//   const [errorMessage, setErrorMessage] = useState(null);
+
+//   const VIDEO_URL = 'http://localhost:3000/videos'
+
+//   useEffect(() => {
+//     axios.get(VIDEO_URL) 
+//       .then((response) => {
+//         const apiVideoList = response.data; 
+//         setVideoList(apiVideoList);
+//       })
+//       .catch((error) => {
+//         setErrorMessage('Could not retrieve videos from library');
+//       });
+//   }, []);
+
+
+    return <h1>This is the library page.</h1>
+}
+
+export default Library; 
