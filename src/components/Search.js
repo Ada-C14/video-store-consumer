799ef7a380326965077c071b04d8f8531adedaef
OlgaSe <olga.tka4eva@gmail.com>
2021-01-20 14:03:49 -0800

    Added searchForm component with search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import SearchForm from './SearchForm';
 
 const Search = () => {
     return (
+        <div>
         <h3>Search page</h3>
+        <SearchForm />
+        </div>
         );
     };
     
