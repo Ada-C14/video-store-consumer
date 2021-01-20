62375cbb9ad4719d3af528cdd07cdca38b85a7ca
Sophie Messing <sophie.e.messing@gmail.com>
2021-01-19 22:18:50 -0800

    can display list of videos from rails api

--- src/Components/VideoLibrary.js
+++ src/Components/VideoLibrary.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 const VideoLibrary = (props) => {
+    
+    const allVideosURL = 'http://localhost:3000/videos'
+
+    const [videos, setVideos] = useState([])
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    useEffect(() => {
+        axios.get(allVideosURL)
+          .then((response) => {
+            const RailsApiVideoList = response.data
+            setVideos(RailsApiVideoList);
+          })
+          .catch((error) => {
+            setErrorMessage(error.message);
+            console.log(error.message);
+          });
+      }, []);
+
+    function VideoList() {
+    
+    const listItems = videos.map((video) =>
+        <li key={video.id}>
+            {video.title}: {video.overview}
+        </li>
+            );
+        return (
+        <ul>{listItems}</ul>
+        );
+    }
+
     return (
-        <p>this is a list of videos</p>
+        <div>
+            <h3>Video Library</h3>
+            <ul>
+                
+            </ul>
+            <VideoList/>
+        </div>
     )
 }
 
