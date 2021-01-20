0db5bc1b9bc8c248d24c52c5f055bca01d173735
Sophie Messing <sophie.e.messing@gmail.com>
2021-01-20 13:47:36 -0800

    merging

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios'
 
-const CustomerList = () => {
-  return  (
-    <h3> Customer List</h3>  
-  )
+const CustomerList = (props) => {
+    
+  const allCustomersURL = 'http://localhost:3000/customers'
+
+  const [customers, setCustomers] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+      axios.get(allCustomersURL)
+      .then((response) => {
+          const RailsApiCustomerList = response.data
+          setCustomers(RailsApiCustomerList);
+      })
+      .catch((error) => {
+          setErrorMessage(error.message);
+          console.log(errorMessage);
+      });
+  }, []);
+
+  function CustomerList() {  
+    console.log(customers)
+    const listItems = customers.map((customer) =>
+        <li key={customer.id}>
+
+            {customer.name}
+            {customer.address}
+        </li>
+        );
+        return (
+        <ul>{listItems}</ul>
+        );
+    }
+
+    return (
+      <div>
+          <h3>All Customers</h3>
+          <h4>Search or Select a Customer: </h4>
+        
+          <CustomerList/>
+      </div>
+  )  
+}
+
+CustomerList.propTypes = {
 
 }
 
-export default CustomerList;
-
+export default CustomerList
