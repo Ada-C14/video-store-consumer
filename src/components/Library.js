5b388f52c3a0d5ce41b5236caf584a9d8caf2200
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-01-20 13:20:24 -0500

    list library works

--- src/components/Library.js
+++ src/components/Library.js
@@ -30,28 +30,50 @@ const Library = (props) => {
   }, []);
 
 
-  const videoLibraryComponents = videoLibrary.map((video) => {
+  const renderTableData = videoLibrary.map((video) => {
+    // const {id, title, overview, releaseDate, inventory} = video
     return (
-      <div key={video.id}> 
-        <Video
-          id={video.id}
-          title={video.title}
-          overview={video.overview}
-          release_date={video.release_date}
-          image_url={video.image_url}
-          external_id={video.external_id}
-        />
-      </div>
+      <tr key={video.id}>
+        <td>{video.title}</td>
+        <td>{video.overview}</td>
+        <td>{video.release_date}</td>
+        {/* <td>{video.inventory}</td> */}
+        {/* <td>{imageUrl}</td>
+        <td>{externalId}</td> */}
+      </tr>
     )
+    // return (
+    //   <div key={video.id}> 
+    //     <Video
+    //       id={video.id}
+    //       title={video.title}
+    //       overview={video.overview}
+    //       release_date={video.release_date}
+    //       image_url={video.image_url}
+    //       external_id={video.external_id}
+    //     />
+    //   </div>
+    // )
   });
 
+
+
+  // const renderTableData = () => {}
+
   
+
   return (
     <div>
-      Library
-      {videoLibraryComponents}
+      <h1>Video Library</h1>
+        <table>
+          <tbody>
+            {renderTableData}
+          </tbody>
+        </table>
     </div>
-  );
+  )
+
 }
 
 export default Library
+
