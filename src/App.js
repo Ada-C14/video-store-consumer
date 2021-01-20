6b563816a33c9ef75a78a117aa4e673041aa63f8
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-19 19:38:58 -0800

    add react router to App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import child components: search, customerlist, videolibrary
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <li>
+                <Link to="/customers">Customers</Link>
+              </li>
+              <li>
+                <Link to="/library">Library</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/customers">
+              <CustomerList />
+            </Route>
+            <Route path="/library">
+              <VideoLibrary />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
