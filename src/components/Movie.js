16c408362071cea2ad26c5573d02cadc4724d8e7
Anna Kim <anna.kim93@gmail.com>
2021-01-20 15:56:32 -0500

    adjust movie to allow for info on click

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
 const Movie = props => {
+  const [showMsg, setShowMsg] = useState(false);
+
+  const handleClick = event => {
+    const popup = document.querySelector('.popup')
+    const closeBtn = document.querySelector('.closeBtn')
+    popup.style.display = 'block';
+    closeBtn.addEventListener('click', () => {
+      popup.style.display = 'none';
+    })
+  };
+
   return (
     <div className='movieCard'>
-      <img src={props.imageURL} alt={props.title}/>
+      { showMsg && (<div>Click for more info</div>) }
+      <img 
+        src={props.imageURL} 
+        alt={props.title} 
+        onMouseEnter={() => setShowMsg(true)} 
+        onMouseLeave={() => setShowMsg(false)}
+        onClick={() => handleClick} 
+      />
       <h5>{props.title}</h5>
       <p>Released: {props.releaseDate}</p>
       <p>Summary: {props.overview}</p>
