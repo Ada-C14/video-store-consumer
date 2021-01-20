d300b1d0ff26f7ee2cae068fd65ff4dcd1317ab6
Jasmine <jasylop@gmail.com>
2021-01-20 12:04:56 -0800

    added logic to show all customers

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect} from 'react';
+// import axios from 'axios';
+import Customer from './Customer';
 
-const Customers = () => {
+const Customers = (props) => {
+  const [customers, setCustomers] = useState([])
+  useEffect( () => {
+      axios.get('http://localhost:3000/customers')
+        .then( (response) => {
+          setCustomers(response.data);
+        })
+        .catch( (error) => {
+          // todo: feature error message on browser
+          console.log(error.message);
+        })
+  }, [] )
+
+  const customerComponents = customers.map(customer => {
+    return(
+      <Customer data={customer} key={customer.id} customerCallback={props.customerCallback} />
+    )
+  })
   return (
     <div>
       <h1>CUSTOMERS</h1>
+      <div>
+        {customerComponents}
+      </div>
     </div>
   )
 }
