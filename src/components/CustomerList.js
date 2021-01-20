6e1d8b02e38271ce445090cb2cfa1ed2d964fa76
Genevieve Neely <genevieve.hood@gmail.com>
2021-01-19 21:26:19 -0800

    render customer list from api call

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Customer from './Customer';
+import axios from 'axios';
 import './CustomerList.css'
 
 const CustomerList = (props) => {
-  const custList = props.list.map((customer) => {
+  const [customers, setCustomers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/customers')
+      .then((response) => {
+        setCustomers(response.data)
+      })
+      .catch((error) => {
+      setErrorMessage(error.message);
+      });
+  },[]);
+
+  const customerComponents = customers.map((customer) => {
     return(< Customer key={customer.id} name={customer.name} />)
   })
+
   return(
     <div className="customer-list">
-      {custList}
+      {customerComponents}
+      { errorMessage ? <div><h2>{errorMessage}</h2></div> : ''}
     </div>
-  )}
+  )};
+
 CustomerList.propTypes = {
-  list: PropTypes.array
+  
 }
 export default CustomerList;
