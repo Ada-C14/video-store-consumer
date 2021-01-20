5c95d3b99e9215cd9bef1206f8fbdc56ff2f2ebc
MonaRahmani <monarahmanimehr@gmail.com>
2021-01-20 14:19:57 -0800

    started working on rental

---
+++ src/components/Rental.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Rental.css';
+import propTypes from 'prop-types';
+
+export default function Rental({movie, customer, rentMovie}) {
+    return (
+        <div>
+        
+        </div>
+    )
+
+}
