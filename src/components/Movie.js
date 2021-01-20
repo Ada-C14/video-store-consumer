479e1a4065c3c0a35e04f4bbed2aca350f12e64e
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-19 18:31:44 -0800

    create MovieList and Movie Components

---
+++ src/components/Movie.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './Movie.css';
+
+const Movie = () => {
+  return (
+    <div><h1>Hello World!</h1></div>
+  );
+}
+
+export default Movie;
---
+++ src/components/MovieList.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './MovieList.css';
+
+const MovieList = () => {
+  return (
+    <div><h1>Hello World!</h1></div>
+  );
+}
+
+export default MovieList;
