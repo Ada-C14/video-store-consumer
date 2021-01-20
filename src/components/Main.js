fd353f6879840dcd0d8621cfd1ce87e42dc05bea
Jasmine <jasylop@gmail.com>
2021-01-20 12:06:19 -0800

    updated routes to allow props

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,8 @@ import Customers from './Customers';
 import Home from './Home';
 import Navigation from './Navigation';
 
-const Main = () => {
+const Main = (props) => {
+  console.log(props.videoCallback)
   //
   //   <Route
   //   path='/dashboard'
@@ -17,8 +18,20 @@ const Main = () => {
     <Switch>
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/search' component={Search}></Route>
-      <Route exact path='/library' component={Library}></Route>
-      <Route exact path='/customers' component={Customers}></Route>
+      {/* <Route exact path='/library' component={Library}></Route> */}
+      <Route
+        exact path='/library'
+        render={() => (
+          <Library videoCallback={props.videoCallback} />
+        )}
+      />
+      {/* <Route exact path='/customers' component={Customers}></Route> */}
+      <Route
+        exact path='/customers'
+        render={() => (
+          <Customers customerCallback={props.customerCallback} />
+        )}
+      />
     </Switch>
   )
 
