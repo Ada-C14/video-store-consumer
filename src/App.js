42a7eaafae7bc6178cfafa89fcf85df72aa74e1c
Jasmine <jasylop@gmail.com>
2021-01-20 09:29:47 -0800

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Library from './components/Library.js';
+import Navigation from './components/Navigation.js';
+import Main from './components/Main.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Library />
+        <Navigation />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import { Switch, Route } from 'react-router-dom';
+import Search from './Search';
+import Library from './Library';
+import Customers from './Customers';
+import Home from './Home';
+import Navigation from './Navigation';
+
+const Main = () => {
+
+  return (
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/search' component={Search}></Route>
+      <Route exact path='/library' component={Library}></Route>
+      <Route exact path='/customers' component={Customers}></Route>
+    </Switch>
+  )
+
+};
+
+export default Main;
