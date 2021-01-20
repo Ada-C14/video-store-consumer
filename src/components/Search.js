5e74b66e3bc896e33b9271c73c08b3b4327e3166
Rachael Gomez <gomezrc1220@gmail.com>
2021-01-20 15:14:29 -0800

    added some more bits to the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,13 @@ import axios from 'axios';
 const Search = () => {
   const [query, setQuery] = useState([]);
   const API_URL_BASE = 'http://localhost:3000'
+
+  searchQuery = (event) => {
+    console.log("new query", event.target.value)
+    setQuery({
+      inputValue: event.target.value
+    })
+  }
   useEffect(() => {
     axios.get(`${API_URL_BASE}/videos?query=<search term>`)
   }, []);
