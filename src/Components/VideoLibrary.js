ddc82fd6e9fe58d035ac4478c65ac47ebde2a2d9
Sophie Messing <sophie.e.messing@gmail.com>
2021-01-20 10:55:39 -0800

    playing with react-select to display video library

--- src/Components/VideoLibrary.js
+++ src/Components/VideoLibrary.js
@@ -1,4 +1,5 @@
 import {React, useState, useEffect} from 'react'
+import Select from 'react-select'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 
@@ -9,36 +10,55 @@ const VideoLibrary = (props) => {
     const [videos, setVideos] = useState([])
     const [errorMessage, setErrorMessage] = useState(null)
 
+    
     useEffect(() => {
         axios.get(allVideosURL)
-          .then((response) => {
+        .then((response) => {
             const RailsApiVideoList = response.data
             setVideos(RailsApiVideoList);
-          })
-          .catch((error) => {
+        })
+        .catch((error) => {
             setErrorMessage(error.message);
-            console.log(error.message);
-          });
-      }, []);
-
-    function VideoList() {
+            console.log(errorMessage);
+        });
+    }, []);
     
+    // const [selectedVideo, setSelectedVideo] = useState(null)
+
+    // const selectVideo = ((video) => {
+    //     setSelectedVideo(video)
+    //     return(
+    //         selectedVideo.title
+    //     )
+    // })
+
+    function VideoList() {  
     const listItems = videos.map((video) =>
         <li key={video.id}>
             {video.title}: {video.overview}
         </li>
-            );
+        );
         return (
         <ul>{listItems}</ul>
         );
     }
 
+
+    const TitleOptions = [ // this is just a test. I want to see if I can map the videos array to display as a select list. Select can also be used as a search bar!
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+      ]
+
+    // const TitleArray = videos.map((video) => 
+    // {value: video.id, label: video.title}
+    //     )
+
     return (
         <div>
             <h3>Video Library</h3>
-            <ul>
-                
-            </ul>
+            <h4>Selected Video: </h4>
+            <Select options={TitleOptions}/>
             <VideoList/>
         </div>
     )
