31dd49b42889d936bae44988fd78127f06a5aef6
christinaminh <christina.minh@gmail.com>
2021-01-20 11:09:06 -0800

    Changed styling for search results to match customer list styling. Commented out incomplete implementation of popups for video overviews

--- src/components/Search/SearchResultItem.js
+++ src/components/Search/SearchResultItem.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 import NewVideoForm from './NewVideoForm'
 import './SearchResultItem.css'
 import Moment from 'react-moment';
+// import onClickOutside from 'react-onclickoutside';
 
 const SearchResultItem = ({item, addVideoCallback}) => {
+  const [selected, setSelected] = useState(false)
+
   const { 
     title, 
     overview, 
@@ -11,20 +14,26 @@ const SearchResultItem = ({item, addVideoCallback}) => {
     image_url: imageUrl, 
   } = item
 
+  const toggleActive = () => {
+      setSelected(!selected)
+  }
+
  return(
-   <div className='search-result-item'>
+   <div className='search-result-item' onClick={(toggleActive)}>
      <img src={imageUrl} alt={`Poster for ${title}`}/>
 
-     <div className='details'>
+     <div className='search-result__content'>
       <h3>{title}</h3>
       <p><Moment format='LL'>{releaseDate}</Moment></p>
-      <p>{overview}</p>
-     </div>
 
-    <div>
-      <NewVideoForm item={item} addVideoCallback={addVideoCallback}/>
-    </div>
+      <div className='add-video-button'>
+        <NewVideoForm item={item} addVideoCallback={addVideoCallback}/>
+      </div>
       
+      {/* <div className={ selected ? 'show-details popup' : null }>
+        <p className='popup__content'>{overview}</p>
+      </div> */}
+     </div>
      
    </div>
  )
