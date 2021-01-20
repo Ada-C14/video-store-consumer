83c82a5e74430173d1adc18778cafc7709443cad
Anna Kim <anna.kim93@gmail.com>
2021-01-20 13:35:06 -0500

    moviesearchresults comp: generateMovieComponents fn

--- src/components/MovieSearchResults.js
+++ src/components/MovieSearchResults.js
@@ -5,6 +5,25 @@ import './MovieSearchResults.css';
 
 const MovieSearchResults = props => {
   console.log(props.location.state)
+
+  const generateMovieComponents = movies => {
+    const currentMovieList = [];
+  
+    for (let movie of movies) {
+      currentMovieList.push(
+        <Movie 
+          key={movie.external_id}  
+          title={movie.title} 
+          overview={movie.overview}
+          releaseDate={movie.release_date}
+          imageURL={movie.image_url}
+        />
+      )
+    }
+  
+    return currentMovieList;
+  };
+
   return (
     <div>
 
