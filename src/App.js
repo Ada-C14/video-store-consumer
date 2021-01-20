14c66ba6f1a8cd2ac0a0f5d67148bc9f89ee56d6
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-01-19 23:49:25 -0800

    import Search to App.js

--- src/App.js
+++ src/App.js
@@ -10,28 +10,12 @@ import {
   useParams
 } from 'react-router-dom';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+// import Home from './components/Home';
+import Search from './components/Search';
+// import Library from './components/Library';
+// import Customers from './components/Customers';
 
-// export default App;
-
-// 'https://api.themoviedb.org/3/search/movie?api_key=' + MOVIEDB_KEY + '=' + movieTitleToSearch
-
-
-export default function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
@@ -73,13 +57,15 @@ export default function App() {
   );
 }
 
+export default App;
+
 function Home() {
   return <h2>Home</h2>;
 }
 
-function Search() {
-  return <h2>Video Search</h2>;
-}
+// // function Search() {
+// //   return <h2>Video Search</h2>;
+// // }
 
 function Customers() {
   return <h2>Customer List</h2>;
