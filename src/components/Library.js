de394340b654e8763202d025697ff64005ccb8d2
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2021-01-20 09:57:48 -0500

    local library rendering not formatted yet

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from 'react-router-dom';
+import React, { useState, useEffect, Component } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useRouteMatch,
+//   useParams
+// } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Video from './Video'
 
 const Library = (props) => {
+  const [videoLibrary, setVideoLibrary] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  let match = useRouteMatch();
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/')
+      .then((response) => {
+        const apiVideoLibrary = response.data;
+        setVideoLibrary(apiVideoLibrary);
+        console.log(videoLibrary)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  }, []);
+
+
+  const videoLibraryComponents = videoLibrary.map((video) => {
+    return (
+      <div key={video.id}> 
+        <Video
+          id={video.id}
+          title={video.title}
+          overview={video.overview}
+          release_date={video.release_date}
+          image_url={video.image_url}
+          external_id={video.external_id}
+        />
+      </div>
+    )
+  });
+
   
   return (
     <div>
-      Library!!
-      <h2>hi! below is a placeholder for nested routes if we need them</h2>
-
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
-
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
+      Library
+      {videoLibraryComponents}
     </div>
   );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
 }
 
 export default Library
