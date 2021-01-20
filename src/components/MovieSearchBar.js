27d15446ae2fc8600015b6238e7d480b3774b3e7
Anna Kim <anna.kim93@gmail.com>
2021-01-20 12:54:46 -0500

    rename components

---
+++ src/components/MovieSearchBar.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import './MovieSearchBar.css';
+
+const MovieSearch = props => {
+  const [formFields, setFormFields] = useState({
+    searchQuery: '',
+    searchResults: []
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  const onInputChange = event => {
+    const newFormFields = { ...formFields };
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  };
+  
+  const onFormSubmit = event => {
+    event.preventDefault();
+
+    const url = props.url + '/videos?query=' + formFields.searchQuery;
+
+    axios.get(url)
+      .then((response) => {
+        const newFormFields = { ...formFields };
+        const newQueryResults = response.data;
+        newFormFields.searchResults = newQueryResults;
+        setFormFields(newFormFields);
+        setAlert(`Found ${formFields.searchResults.length} results for your search:`);
+      })
+      .catch((error) => {
+        setAlert(error.message);
+      })
+  };
+
+  return (
+    <div>
+      <form onSubmit={onFormSubmit}>
+        <input name='searchQuery' onChange={onInputChange} value={formFields.searchQuery} placeholder='Search for a movie' />
+        <button type='submit' value='Search'>Search</button>
+      </form>
+      { formFields.searchResults.length > 0 &&
+        <Redirect to={{
+          pathname: '/results',
+          state: { results: this.state.results }
+        }}/>
+      }
+    </div>
+  );
+};
+
+MovieSearch.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default MovieSearch;
