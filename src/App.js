e7024706e659cbc576d1b5871b196a67da0ee98d
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-19 21:19:08 -0800

    install react-router, Switches between different routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  Link,
   Route,
-  Link
 } from 'react-router-dom';
+import { Switch } from 'react-router';
 import VideoList from './components/VideoList';
 
 class App extends Component {
@@ -24,14 +24,15 @@ class App extends Component {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            <li>
+              <Link to="/videos">Videos</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
-          <div className="App">
-            <Route path="/">
-              <VideoList />
-            </Route>
-          </div>
+          <Route path="/videos">
+            <VideoList />
+          </Route>
         </Switch>
       </Router>
     );
