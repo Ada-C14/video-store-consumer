400f8a9b436dd8636b6c586d8aba625380749949
Jasmine <jasylop@gmail.com>
2021-01-20 12:04:25 -0800

    added logic to show when a video and customer are selected

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,3 +1,18 @@
+import React from 'react';
+
+const Checkout = (props) => {
+  const {video, customer} = props
+  return (
+    <div className='checkout'>
+      {video && <h3>Video Selected: {video.title}</h3>}
+      {customer && <h3>Customer Selected: {customer.name}</h3>}
+      {(video && customer) && <button>CHECKOUT</button>}
+    </div>
+  )
+}
+
+export default Checkout;
+
 // If there is a video saved to state
 // Show text or something "You selected video: [video]"
 // some sort of indicator to show that a video has been selected
