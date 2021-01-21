48ac73fce1623a7898c0c7ef36031b84090a8b61
Genevieve Neely <genevieve.hood@gmail.com>
2021-01-21 10:59:15 -0800

    remove unused import

--- src/components/VideoLibrary/Video.js
+++ src/components/VideoLibrary/Video.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Moment from 'react-moment';
 import { Button } from 'react-bootstrap'
 import './Video.css'
-import { propTypes } from 'react-bootstrap/esm/Image';
-import { useLocation } from 'react-router-dom';
 import NewVideoForm from '../Search/NewVideoForm'
 
 
@@ -37,16 +35,11 @@ const Video = ({ currentPathname, video, setVideo, addVideoCallback }) => {
 
   return (
     <div className="video">
- 
       <img src={imageUrl} alt={`Poster for ${title}`} />
 
       <div className='video__content'>
         <h3>{title}</h3>
         <p><Moment format='LL'>{releaseDate}</Moment></p>
-
-        {/* <div className={ selected ? 'show-details popup' : null }>
-          <p className='popup__content'>{overview}</p>
-        </div> */}
       </div>
 
       <div>
