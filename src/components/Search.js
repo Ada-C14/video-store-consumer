cbc4200e569dfc1d4022740f830ef6d7ea9f3769
OlgaSe <olga.tka4eva@gmail.com>
2021-01-20 22:24:03 -0800

    Added checkboxes for videos, and selectVideo,getSelectedVideos, addToLibrary to collect selected videos and send them to the backend

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './SearchForm';
 import Video from './Video';
 import PropTypes from 'prop-types';
@@ -10,6 +10,7 @@ const Search = (props) => {
 
     const [videosList, setVideosList] = useState([]);
     const [errorMessage, setErrorMessage] = useState(null);
+    const [selectedVideosIds, setSelectedVideosIds] = useState({});
 
     // useEffect(() => {
     //     axios.get(API_URL_BASE)
@@ -32,20 +33,62 @@ const Search = (props) => {
             setErrorMessage(error.message);
         });
     }
+    //collect ids of videos that were selected
+    const selectVideo = (event) => {
+        const selectedVideosIdsNew = {...selectedVideosIds};
+
+        selectedVideosIdsNew[event.target.value] = event.target.checked;
+        //value = video id, if video was checked than value will be true
+        setSelectedVideosIds(selectedVideosIdsNew);
+    }
+
+    //filtering video from videosList, that has corresponding id in selectedVideosIds
+    const getSelectedVideos = () => {
+        return videosList.filter((video) => selectedVideosIds[video.external_id]);
+    }
+
+    // save selected videos and send them (as array within videos obj) as a parameter to the api
+    const addToLibrary = () => {
+        const selectedVideos = getSelectedVideos();
+
+        axios.post(API_URL_BASE + 'videos', {videos: selectedVideos})
+        .then((response) => {
+            setSelectedVideosIds({});
+        })
+        .catch((error) => {
+            setErrorMessage(error.message);
+        });
+
+    }
 
     const videosComponentsList = videosList.map((video) => {
-        return <Video
+        return <div key={`div${video.external_id}`}>
+            <input type="checkbox" 
+            value={video.external_id} //value of the checkbox is video id
+            // check if selectedVideosIds contains video with corresponding id with true value
+            checked={selectedVideosIds[video.external_id]?true:false} 
+            onChange={selectVideo}
+            />
+        <Video
         title={video.title}
         overview={video.overview}
         release_date={video.release_date}
         image_url={video.image_url}
         id={video.external_id}
-        key={video.id}
+        key={video.external_id}
         />
+        </div>
     })
     return (
         <div>
         <SearchForm onSubmitCallback={searchVideo} />
+        {/* if nothing was selected the button is disabled */}
+        <button 
+        disabled={getSelectedVideos().length===0}
+        onClick={addToLibrary}
+        > 
+            Add selected to the library
+        </button>
         {errorMessage ? <div>{errorMessage}</div> : '' }
         {videosComponentsList}
         </div>
