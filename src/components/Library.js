a2097d4130a67d41c3dcef21dec96b05c5417379
Alice Boone <alicegbone@gmail.com>
2021-01-21 15:01:03 -0800

    library component, video list

--- src/components/Library.js
+++ src/components/Library.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import './Library.css';
 
+const baseURL = 'http://localhost:3000'
+
 const Library = (props) => {
   const [videoList, setVideoList] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000')
+    axios.get(baseURL)
       .then(response => {
         setVideoList(response.data);
       })
@@ -17,20 +19,24 @@ const Library = (props) => {
   }, []);
 
   return (
-    <div className="video-container">
-      {videoList.map((video) => (
-        <React.Fragment key={video.id}>
-          <button
-            onClick={() => props.onSelectVideoCallback(video)}>Select</button>
-          <div>{video.name}</div>
-        </React.Fragment>
-      ))
-      }
+    <div>
+      <h2> Videos Library</h2>
+      <table className="videos-table">
+        {videoList.map((video) => (
+          <React.Fragment key={video.id}>
+            <tr></tr>
+            <td className='videos-table-title'>{video.title}</td>
+            <td className='videos-table-select'><button onClick={() => props.onSelectVideoCallback(video)}>Select</button></td>
+          </React.Fragment>
+        ))
+        }
+      </table>
     </div>
   )
 }
 
 Library.propTypes = {
   onSelectVideoCallback: PropTypes.func.isRequired,
+videoList: PropTypes.array.isRequired
 }
 export default Library;
