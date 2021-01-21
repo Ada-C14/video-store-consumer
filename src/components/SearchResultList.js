ef09f45eb0c62fac047d97e002404bf4dfd2824c
Ringo Alcock <ringo.alcock@gmail.com>
2021-01-20 22:17:18 -0800

    define addFilm function and update SearchResultList table render

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 const SearchResultList = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [error, setError] = useState('');
+    const [alert, setAlert] = useState('');
 
     const searchSubmit = (title) => {   
         // TODO pass URL down in props?
@@ -24,13 +25,35 @@ const SearchResultList = () => {
             });
     };
 
+    const addFilm = (newFilm) => {
+        // TODO pass base URL down in props
+        axios.post(`http://localhost:3000/videos`, newFilm)
+            .then((response) => {
+                // TODO - display a message to let user know
+                // film successfully added?
+                setAlert(`You have added ${newFilm.title} to your library`);
+                setError('');
+            })
+            .catch((error) => {
+                // TODO - display error message?
+                console.log(error.response)
+                setError(error.response.data.errors);
+                setAlert('');
+            });
+    };
+
     const buildResults = searchResults.map((video) => {
         return (
             <Video
-                // key={video.external_id}
+                key={video.external_id}
                 title={video.title}
                 imageUrl={video.image_url}
                 overview={video.overview}
+                releaseDate={video.release_date}
+                externalId={video.external_id}
+                buttonClass="add-film"
+                buttonText="ADD"
+                addFilmCallback={addFilm}
             />
         )
     })
@@ -38,15 +61,17 @@ const SearchResultList = () => {
     return (
         <div>
             <SearchForm onSubmitCallback={searchSubmit} />
-            { error ? error : '' }
+            {/* TODO - style errors and alerts to be more obvious */}
+            <div>{ error ? error : '' }</div>
+            <div>{ alert ? alert : '' }</div>
             <h3>Search Results</h3>
-            <table>
+            <table className="table">
                 <thead>
                     <tr>
                         <th></th>
-                        <th>TITLE</th>
-                        <th>IMAGE</th>
-                        <th>OVERVIEW</th>
+                        <th className="tableHeader">TITLE</th>
+                        <th className="tableHeader">ADD TO LIBRARY</th>
+                        <th className="tableHeader">OVERVIEW</th>
                     </tr>
                 </thead>
                 <tbody>
