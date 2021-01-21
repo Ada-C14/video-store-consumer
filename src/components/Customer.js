548d5ec903313f6c4a4cb330d4f474368b60c4d4
Blaine <b.atsbeha@gmail.com>
2021-01-21 15:17:39 -0800

    setting up selected customer state and passing through props

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,12 +1,14 @@
 const Customer = (props) => {
+
+  const customer = props.customer
   return (
 
     <tr class="table-active">
-        <th scope="row">{props.name}</th>
-        <td>{props.phone}</td>
-        <td>{props.accountCredit}</td>
-        <td>{props.videosCheckedOutCount}</td>
-        <td><button type="button" class="btn btn-outline-primary">Select!</button></td>
+        <th scope="row">{customer.name}</th>
+        <td>{customer.phone}</td>
+        <td>{customer.accountCredit}</td>
+        <td>{customer.videosCheckedOutCount}</td>
+        <td><button onClick={() => (props.selectedCustomerCallback(customer))} class="btn btn-outline-primary">Select!</button></td>
     </tr>
 )
 }
