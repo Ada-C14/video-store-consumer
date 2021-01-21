c2182506430960ee13d6a638c5c3d171717cfccb
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-01-20 16:26:29 -0800

    forgot to add updated Customer.js file

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import Customer from './Customer'
 
-const Customers = () => {
+const Customers = (props) => {
+    
+
+    const [customers,setCustomersList] = useState([]);
+    const [errorMessage,setErrorMessage] = useState(null);
+
+    // this will get us a list of customers from API and update the state
+    useEffect(() => {
+        axios.get('http://localhost:3000/customers')
+        .then((response) => {
+            setCustomersList(
+                response.data
+            );
+        })
+        .catch((error) => {
+            setErrorMessage(error.message);
+        });
+    }, []);
+
+        // taking customer obj and turning into customer component 
+    const CustomerComponents = customers.map((customerInfo) => {
+        return (< Customer 
+            id={customerInfo.id}
+            name={customerInfo.name}
+            account_credit={customerInfo.account_credit}
+            movie_count={customerInfo.movie_count}
+            key={customerInfo.id}
+            onClickCallback={props.onClickCallback}
+            />)
+        })
+
+
     return (
-        <h3>Customers page</h3>
+        <div className= 'Customers'>
+            <ul className= "customers-list" >
+            <h3> All Customers</h3>
+            {CustomerComponents}
+            </ul>
+        </div>
         );
     };
     
+
 export default Customers;
