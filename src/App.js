6c1ba1ff93f67499f8f80b08f3a66431f1ac9ca9
Alice Boone <alicegbone@gmail.com>
2021-01-20 17:15:27 -0800

    Add select video and customer to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Customers from './components/Customers';
+import Library from './components/Library';
+import Search from './components/Search';
+import Home from './components';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+function AppRouter() {
+
+  const [selectCustomer, setSelectCustomer] = useState(undefined);
+  const [selectVideo, setSelectVideo] = useState(undefined);
+  
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/search">Search</Link></li>
+            <li><Link to="/library">Library</Link></li>
+            <li><Link to="/customers">Customers</Link></li>
+          </ul>
+        </nav>
+
+        {selectCustomer && <div>Selected Customer: {selectCustomer.name}</div>}
+        {selectVideo && <div>Selected Video: {selectVideo.name}</div>}
+
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/library">
+          <Library onSelectVideoCallback={setSelectVideo} />
+          </Route>
+          <Route path="/customers">
+            <Customers onSelectCustomerCallback={setSelectCustomer} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+
+export default AppRouter;
