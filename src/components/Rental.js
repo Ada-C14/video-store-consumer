7eb3e86c74eec8ddd982e525f63d5155af0ce3bb
MonaRahmani <monarahmanimehr@gmail.com>
2021-01-20 17:14:21 -0800

    worked on rental- not done

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -5,7 +5,9 @@ import propTypes from 'prop-types';
 export default function Rental({movie, customer, rentMovie}) {
     return (
         <div>
-        
+            { movie && <p>selected movie: { movie.title }, { movie.release_date }</p> }
+            { customer && <p>selected customer: { customer.name } </p> }
+            { (movie && customer) && <button onClick={() => { rentMovie()}}>Rent Movie</button> }
         </div>
     )
 
