6efb61b4fe2bfd79e750fef42c728b9dd1a0f8d6
beauttie <bak02013@pomona.edu>
2021-01-20 22:25:28 -0800

    Use React Router to render components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import VideoSearch from './components/VideoSearch';
+import VideoLibrary from './components/VideoLibrary';
+import Customers from './components/Customers';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  const API_BASE_URL = 'http://localhost:3000';
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Video Search</Link>
+            </li>
+            <li>
+              <Link to="/library">Video Library</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/search'>
+            <VideoSearch url={API_BASE_URL}/>
+          </Route>
+          <Route path='/library'>
+            <VideoLibrary url={API_BASE_URL}/>
+          </Route>
+          <Route path="/customers">
+            <Customers url={API_BASE_URL}/>
+          </Route>
+          <Route path="/">
+            Welcome
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
