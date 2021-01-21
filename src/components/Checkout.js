dba74d7d754eae72112d7a1ec7ff7fd9d0510471
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-21 14:40:54 -0800

    edit indentation

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,15 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-
-
 const BASE_URL = 'http://localhost:3000/rentals';
-
 const axios = require('axios');
 
 const Checkout = (props) => {
-
   const [CheckoutMessage, setCheckoutMessage] = useState('');
-
   const [CustomerAndVideo,setCustomerAndVideo] = useState({
     video: '',
     customer: ''
@@ -22,9 +17,7 @@ const Checkout = (props) => {
       video: props.video,
       customer: props.customer.name
     });
-
   }
-
 
   useEffect(() => {
 
@@ -36,35 +29,29 @@ const Checkout = (props) => {
     newDueDate.setDate(newDueDate.getDate() + 7 );
 
     axios.post(BASE_URL + CustomerAndVideo.video + '/check-out', {
-      
         customerId:CustomerAndVideo.customer,
         dueDate: newDueDate,
-
     })
 
-      .then((response) => {
-        setCheckoutMessage(props.customer.name + ' checked out: ' + CustomerAndVideo.video +'!' + 'Due Date' + newDueDate.format('LL') );
-        props.setDisplayMessage({message:props.customer.name + ' Successfully Checked Out Video ' + CustomerAndVideo.video , severity: 'success'});
-        console.log('Successfully Checked Out Video' + CustomerAndVideo.video);
-      })
-      .catch((error)=>{
-        // props.setDisplayMessage({message: 'Failed to check out video', severity: 'error'});
-        console.log('FAILED ON API CALL')
-      });
-      ;
-
+    .then((response) => {
+      setCheckoutMessage(props.customer.name + ' checked out: ' + CustomerAndVideo.video +'!' + 'Due Date' + newDueDate.format('LL') );
+      props.setDisplayMessage({message:props.customer.name + ' Successfully Checked Out Video ' + CustomerAndVideo.video , severity: 'success'});
+      console.log('Successfully Checked Out Video' + CustomerAndVideo.video);
+    })
+    .catch((error)=>{
+      // props.setDisplayMessage({message: 'Failed to check out video', severity: 'error'});
+      console.log('FAILED ON API CALL')
+    });
   }, [CustomerAndVideo]);
 
-
-
-
-
-  return <span>
-    <button className='Checkout-checkIn-checkOut MainButton' onClick = {onSubmitCheckout}>
-    Check Out Video
-    </button>
-    <p>{CheckoutMessage}</p>
+  return(
+    <span>
+      <button className='Checkout-checkIn-checkOut MainButton' onClick = {onSubmitCheckout}>
+      Check Out Video
+      </button>
+      <p>{CheckoutMessage}</p>
     </span>
+  );
 }
 
-export default Checkout
+export default Checkout;
