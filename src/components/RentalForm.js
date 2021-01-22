075c0a06ddcb439af5f0161863a61c0ffd86db79
Taylor Tompkins <taylormililani@gmail.com>
2021-01-21 16:32:30 -0800

    finally figured out how to format a date after what felt like 7 years

--- src/components/RentalForm.js
+++ src/components/RentalForm.js
@@ -3,27 +3,38 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const RentalForm = (props) => {
+    const setDueDate = () => {
+        
+        // let date = new Date()
+        // date.setDate(date + 7)
+        // return date.toISOString().split('T')[0]
+        const date = new Date(new Date(new Date().setDate(new Date().getDate()+7)).toString().split('GMT')[0]+' UTC').toISOString().split('T')[0]
+        return date
+    }
+
+    const checkout = (props, duedate) => {
+        axios.post(`${props.url}/rentals/${props.selectedVideo.title}/check-out?customer_id=${props.selectedCustomer.id}&due_date=`).then()
+    }
+
+
     const form = 
     <h1>form</h1>
 
     const error = 
     <h1>Please Select a Video and Customer to make a rental!</h1>
 
-
     const validRental = (props.selectedCustomer === null || props.selectedVideo === null ) ? error : form
-    
-    
-    
-    
     
     return (
         <div>
             {validRental}
+            {setDueDate()}
         </div>
     );
 }
 
 RentalForm.propTypes = {
+    url: PropTypes.string.isRequired,
     selectedCustomer: PropTypes.object.isRequired,
     selectedVideo: PropTypes.object.isRequired
 }
