2790c343c6efabf46f0ab725ce8d3674803191ec
dnsrocha <drocha@gmail.com>
2021-01-21 16:35:25 -0800

    removed repeated video details statement

--- src/components/SearchDetail.js
+++ src/components/SearchDetail.js
@@ -4,7 +4,6 @@ const SearchDetail = (props) => {
     if (!props.video.title) return null;
 
     return <div>
-        <h5>Video Details:</h5>
         <p>Title: {props.video.title}, Release Date: {props.video.release_date}</p>
         <p>Overview: {props.video.overview}</p>
         <img src = {props.video.image_url} alt="alt"/>
