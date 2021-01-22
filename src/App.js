53f35aa25921b0af2b23322272af2193faf00b31
Stacy <lundquist.stacy@gmail.com>
2021-01-21 19:32:47 -0800

    trying Kareha's idea

--- src/App.js
+++ src/App.js
@@ -48,12 +48,17 @@ class App extends Component {
   checkoutVideo = () => {
     if (this.state.chosenVideo && this.state.chosenCustomer) {
       const title = this.state.chosenVideo.title
-      const dueDate = moment().add(7, 'days').format('YYYY-DD-MM');
+      // const dueDate = moment().add(7, 'days').format('YYYY-MM-DD');
+      const getDueDate = () => {
+        const date = new Date();
+        date.setDate(date.getDate() + 7);
+        return (moment(date).format('MMM, D, YYYY'))
+      }
 
       const params = {
         customerID: this.state.chosenCustomer.id,
         videoID: this.state.chosenVideo.id,
-        dueDate: dueDate,
+        dueDate: getDueDate(),
       }
     
       const checkoutURL = this.API_URL + `/rentals/${title}/check-out`;
