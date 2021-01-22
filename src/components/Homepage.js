ac811de36bef55ed284f95faeb9252bd9049364d
Marj E <marj@marjorieetienne.com>
2021-01-22 06:02:49 -0800

    added emojis to homepage and changed wording

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 import './Homepage.css';
 
+const EMOJI_LIST = ['🤨', '😒', '🙄', '🤯', '😤', '😡', '😠', '🤬', '👿', '💢', '😾', '😑', '😩', '🤦', '😱', '😧']
+// eslint-disable-next-line no-extend-native
+Array.prototype.sample = function(){
+  return this[Math.floor(Math.random()*this.length)];
+}
+
 const Homepage = (props) => {
   const calcDaysOverdue = (dueDate) => {
     return Math.floor((Date.now() - Date.parse(dueDate))/(1000*60*60*24))
   }
   const overdueList = props.overdue.map((person) => {
-    return <p className='overdue-text'>{person.name} is {calcDaysOverdue(person.due_date)} days overdue >:| </p>
+    const daysOverdue = calcDaysOverdue(person.due_date)
+    return <p className='overdue-text'>{EMOJI_LIST.sample()} <b>{person.name}</b> still has <b>{person.title}</b> which is <b>{daysOverdue} {daysOverdue > 1 ? 'days' : 'day'} </b> overdue {EMOJI_LIST.sample()} </p>
   })
 
   return (
