c5ff81f97000e4ff007663e13ec5980b6fe46297
Mackenzie <mvlofthus@gmail.com>
2021-01-21 16:43:54 -0800

    added images to buttons

--- src/components/LibraryEntry.js
+++ src/components/LibraryEntry.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 // This returns an entry in the video library: title and id
 
 const LibraryEntry = (props) => {
-  return (<button
-    onClick={() => {props.libraryCallback(
-      {id: props.id, 
-      title: props.title}
-      )
+  return (
+  <div> 
+    
+    <p>
+    <button 
+      onClick={() => {props.libraryCallback(
+        {id: props.id, 
+        title: props.title}
+        )
+      }
     }
-  }
-  > 
-    {props.title}
-
+    > 
+      <img src={props.image_url} alt={props.title}/>
+      <p>Select {props.title}</p>
     </button>
-
+    </p>
+    </div>
   )
 }
 
